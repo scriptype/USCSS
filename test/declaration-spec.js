3b4a34f7cbbeb5b2e8c5bb47040e7207ad46ac5b
Mustafa Enes Ertarhanaci <scriptyper@gmail.com>
2014-05-18 20:54:15 +0300

    More comments & tests added

--- test/declaration-spec.js
+++ test/declaration-spec.js
@@ -5,7 +5,16 @@ var source      = (new Buffer(FileSystem.readFileSync("./demo/style.uscss"))).to
     declarations = Utils.getDeclarations(source)
 
 /* BUGINDEX
- * TODO #1: fix comments, https://github.com/scriptype/USCSS/issues/1 */
+ * TODO #1: fix comments, https://github.com/scriptype/USCSS/issues/1
+ * TODO #2: fix rule splitting, https://github.com/scriptype/USCSS/issues/2 */
+
+describe("Declaration splitting", function () {
+
+    it("should be resulted with 6 declarations in total", function () {
+        expect(declarations.length).toBe(6) // TODO #1
+    })
+
+})
 
 describe("Declaration class", function () {
 
@@ -46,10 +55,33 @@ describe("Declaration class", function () {
         expect(declarations[1].rulesObject["background"].replace(/\s/g, "")).toBe("[(url(..img/leyley.png)topleft,url(../img/lel.png)bottomcenter),url(../img/lol.png),#fd5]")
 
         expect(declarations[4].rulesObject["color"]).toBe("red") // TODO #1
-        expect(declarations[4].rulesObject["background"]).toBe("[blue, red]")
+        expect(declarations[4].rulesObject["background"]).toBe("[blue, red]") // TODO #2
 
         expect(declarations[5].rulesObject["color"]).toBe("red")
-        expect(declarations[5].rulesObject["background"]).toBe("[blue, red]")
+        expect(declarations[5].rulesObject["background"]).toBe("[blue, red]") // TODO #2
+    })
+
+    it("should allow multiline values", function () {
+        expect(declarations[0].rulesObject["border"].replace(/\s/g, "")).toBe("[1pxsolidred,2pxsolidrgba(255,170,50,.5),2pxdashedblue,5pxsolidblack]")
+        expect(declarations[1].rulesObject["background"].replace(/\s/g, "")).toBe("[(url(..img/leyley.png)topleft,url(../img/lel.png)bottomcenter),url(../img/lol.png),#fd5]")
+    })
+
+    it("should get $states properly", function () {
+        expect(declarations[0]["states"][0]).toBe("")
+        expect(declarations[0]["states"][1]).toBe(":hover")
+        expect(declarations[0]["states"][2]).toBe(":active .inner-element")
+        expect(declarations[0]["states"][3]).toBe(".small")
+
+        expect(declarations[1]["states"][0]).toBe("")
+        expect(declarations[1]["states"][1]).toBe(":active")
+        expect(declarations[1]["states"][2]).toBe(":hover")
+        expect(declarations[1]["states"][3]).toBe(".big")
+
+        expect(declarations[4]["states"][0]).toBe("")
+        expect(declarations[4]["states"][1]).toBe(":not(.hede)")
+
+        expect(declarations[5]["states"][0]).toBe("")
+        expect(declarations[5]["states"][1]).toBe(":last-child")
     })
 
     it("should delete pseudo-property $state from output", function () {
@@ -60,8 +92,89 @@ describe("Declaration class", function () {
         expect(declarations[5].rulesObject["$states"]).toBeUndefined()
     })
 
-    it("should allow multiline values", function () {
-        expect(declarations[0].rulesObject["border"].replace(/\s/g, "")).toBe("[1pxsolidred,2pxsolidrgba(255,170,50,.5),2pxdashedblue,5pxsolidblack]")
-        expect(declarations[1].rulesObject["background"].replace(/\s/g, "")).toBe("[(url(..img/leyley.png)topleft,url(../img/lel.png)bottomcenter),url(../img/lol.png),#fd5]")
+    it("should set STATE-RULES properly", function () {
+        expect(declarations[0].stateRules[0]["state"]).toBe("")
+        expect(declarations[0].stateRules[0]["rules"]["font-family"]).toBe("helvetica, arial, sans-serif")
+        expect(declarations[0].stateRules[0]["rules"]["display"]).toBe("block")
+        expect(declarations[0].stateRules[0]["rules"]["text-decoration"]).toBe("none")
+        expect(declarations[0].stateRules[0]["rules"]["color"]).toBe("blue")
+        expect(declarations[0].stateRules[0]["rules"]["margin"]).toBe("0")
+        expect(declarations[0].stateRules[0]["rules"]["font-size"]).toBe("1em")
+        expect(declarations[0].stateRules[0]["rules"]["background"]).toBe("red")
+        expect(declarations[0].stateRules[0]["rules"]["border"]).toBe("1px solid red")
+
+        expect(declarations[0].stateRules[1]["state"]).toBe(":hover")
+        expect(declarations[0].stateRules[1]["rules"]["font-family"]).toBeUndefined()
+        expect(declarations[0].stateRules[1]["rules"]["display"]).toBeUndefined()
+        expect(declarations[0].stateRules[1]["rules"]["text-decoration"]).toBeUndefined()
+        expect(declarations[0].stateRules[1]["rules"]["color"]).toBe("red")
+        expect(declarations[0].stateRules[1]["rules"]["margin"]).toBe("0 50px")
+        expect(declarations[0].stateRules[1]["rules"]["font-size"]).toBe("1em")
+        expect(declarations[0].stateRules[1]["rules"]["background"]).toBe("rgba(255,255,255,.5)")
+        expect(declarations[0].stateRules[1]["rules"]["border"]).toBe("2px solid rgba(255,170,50,.5)")
+
+        expect(declarations[0].stateRules[2]["state"]).toBe(":active .inner-element")
+        expect(declarations[0].stateRules[2]["rules"]["font-family"]).toBeUndefined()
+        expect(declarations[0].stateRules[2]["rules"]["display"]).toBeUndefined()
+        expect(declarations[0].stateRules[2]["rules"]["text-decoration"]).toBeUndefined()
+        expect(declarations[0].stateRules[2]["rules"]["background"]).toBeUndefined()
+        expect(declarations[0].stateRules[2]["rules"]["color"]).toBeUndefined()
+        expect(declarations[0].stateRules[2]["rules"]["margin"]).toBe("0 25px")
+        expect(declarations[0].stateRules[2]["rules"]["font-size"]).toBe("1.1em")
+        expect(declarations[0].stateRules[2]["rules"]["border"]).toBe("2px dashed blue")
+
+        expect(declarations[0].stateRules[3]["state"]).toBe(".small")
+        expect(declarations[0].stateRules[3]["rules"]["font-family"]).toBeUndefined()
+        expect(declarations[0].stateRules[3]["rules"]["display"]).toBeUndefined()
+        expect(declarations[0].stateRules[3]["rules"]["text-decoration"]).toBeUndefined()
+        expect(declarations[0].stateRules[3]["rules"]["background"]).toBeUndefined()
+        expect(declarations[0].stateRules[3]["rules"]["color"]).toBeUndefined()
+        expect(declarations[0].stateRules[3]["rules"]["margin"]).toBe("10px")
+        expect(declarations[0].stateRules[3]["rules"]["font-size"]).toBe(".8em")
+        expect(declarations[0].stateRules[3]["rules"]["border"]).toBe("5px solid black")
+
+        // --------------------------------------------------------------------------------
+
+        expect(declarations[1].stateRules[0]["state"]).toBe("")
+        expect(declarations[1].stateRules[0]["rules"]["font-family"]).toBe("helvetica, arial, sans-serif") // TODO #1
+        expect(declarations[1].stateRules[0]["rules"]["display"]).toBe("block")
+        expect(declarations[1].stateRules[0]["rules"]["text-decoration"]).toBe("none")
+        expect(declarations[1].stateRules[0]["rules"]["color"]).toBe("#eee") // TODO #1
+        expect(declarations[1].stateRules[0]["rules"]["background"]).toBe("url(..img/leyley.png) top left, url(../img/lel.png) bottom center")
+
+        expect(declarations[1].stateRules[1]["state"]).toBe(":active")
+        expect(declarations[1].stateRules[1]["rules"]["font-family"]).toBeUndefined()
+        expect(declarations[1].stateRules[1]["rules"]["text-decoration"]).toBeUndefined()
+        expect(declarations[1].stateRules[1]["rules"]["display"]).toBe("none")
+        expect(declarations[1].stateRules[1]["rules"]["color"]).toBe("#ddd") // TODO #1
+        expect(declarations[1].stateRules[1]["rules"]["background"]).toBe("url(../img/lol.png)")
+
+        expect(declarations[1].stateRules[2]["state"]).toBe(":hover")
+        expect(declarations[1].stateRules[2]["rules"]["font-family"]).toBeUndefined()
+        expect(declarations[1].stateRules[2]["rules"]["text-decoration"]).toBeUndefined()
+        expect(declarations[1].stateRules[2]["rules"]["display"]).toBe("block")
+        expect(declarations[1].stateRules[2]["rules"]["color"]).toBe("red") // TODO #1
+        expect(declarations[1].stateRules[2]["rules"]["background"]).toBe("#fd5")
+
+        // --------------------------------------------------------------------------------
+
+        expect(declarations[4].stateRules[0]["state"]).toBe("")
+        expect(declarations[4].stateRules[0]["rules"]["color"]).toBe("red") // TODO #1
+        expect(declarations[4].stateRules[0]["rules"]["background"]).toBe("blue")
+
+        expect(declarations[4].stateRules[1]["state"]).toBe(":not(.hede)")
+        expect(declarations[4].stateRules[1]["rules"]["color"]).toBeUndefined()
+        expect(declarations[4].stateRules[1]["rules"]["background"]).toBe("red")
+
+        // --------------------------------------------------------------------------------
+
+        expect(declarations[5].stateRules[0]["state"]).toBe("")
+        expect(declarations[5].stateRules[0]["rules"]["color"]).toBe("red")
+        expect(declarations[5].stateRules[0]["rules"]["background"]).toBe("blue")
+
+        expect(declarations[5].stateRules[1]["state"]).toBe(":last-child")
+        expect(declarations[5].stateRules[1]["rules"]["color"]).toBeUndefined()
+        expect(declarations[5].stateRules[1]["rules"]["background"]).toBe("red")
+
     })
 })
